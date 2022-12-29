4694a5469618e91af1e31a3a264c644e725b3820
LeProxy#0542 <104642778+DX9Paster@users.noreply.github.com>
2022-12-28 17:31:06 -0800

    Update example.hpp

--- example.hpp
+++ example.hpp
@@ -3,7 +3,7 @@
 	Fortnite Silent Aim Externally Example
 	https://github.com/DX9Paster
   
-  Copyright (c) 2023 DX9Paster
+        Copyright (c) 2023 DX9Paster
 	Permission is hereby granted, free of charge, to any person
 	obtaining a copy of this software and associated documentation
 	files (the "Software"), to deal in the Software without
@@ -25,39 +25,58 @@
  */
 
 bool silentaim = false;
-Vector3 Location;
-Location = Fortnites Camera Location, Found in W2S.
-  
 DWORD_PTR ClosestPawn = NULL;
-PlayerController = Read<DWORD_PTR>(Localplayers + 0x30);
+Vector3 Location; // Camera Location
+
+Vector3 CalculateNewRotation(Vector3& zaz, Vector3& daz) {
+	Vector3 dalte = zaz - daz;
+	Vector3 ongle;
+	float hpm = sqrtf(dalte.x * dalte.x + dalte.y * dalte.y);
+	ongle.y = atan(dalte.y / dalte.x) * 57.295779513082f;
+	ongle.x = (atan(dalte.z / hpm) * 57.295779513082f) * -1.f;
+	if (dalte.x >= 0.f) ongle.y += 180.f;
+	return ongle;
+}
+
+Vector3 GetLoc(Vector3 Loc) {
+	Vector3 Location = Vector3(Loc.x, Loc.y, Loc.z);
+	return Location;
+}
 
 if (silentaim && GetAsyncKeyState(VK_RBUTTON))
+{
+	uint64_t currentactormesh = Read<uint64_t>(closestPawn + 0x310);
+	Vector3 TargetPosition = GetBoneWithRotation(currentactormesh, 66);
+	Vector3 TargetPositionToScreen = ProjectWorldToScreen(GetLoc(TargetPosition));
+
+	uintptr_t CurrentWeapon = Read<uintptr_t>(LocalPawn + 0x8d8);
+	uintptr_t PlayerCameraManager = Read<uintptr_t>(PlayerController + 0x340);
+	
+	uintptr_t ViewPitchMin = 0x331c; // APlayerCameraManager - ViewPitchMin
+	uintptr_t ViewPitchMax = 0x3320; // APlayerCameraManager - ViewPitchMax
+	
+        if (TargetPositionToScreen.x != 0 || TargetPositionToScreen.y != 0 || TargetPositionToScreen.z != 0)
 	{
-			uint64_t currentactormesh = Read<uint64_t>(closestPawn + 0x310);
-			Vector3 TargetPosition = GetBoneWithRotation(currentactormesh, 66);
-			Vector3 TargetPositionToScreen = ProjectWorldToScreen(TargetPosition);
+		if (CurrentWeapon && PlayerCameraManager)
+		{
+			Vector3 NewRotation = CalculateNewRotation(Location, TargetPosition);
 
-			uintptr_t CurrentWeapon = Read<uintptr_t>(LocalPawn + 0x8d8);
-			uintptr_t PlayerCameraManager = Read<uintptr_t>(PlayerController + 0x340);
-
-			if (TargetPositionToScreen.x != 0 || TargetPositionToScreen.y != 0 || TargetPositionToScreen.z != 0)
-			{
-				if (CurrentWeapon && PlayerCameraManager)
-				{
-					Vector3 NewRotation = CalculateNewRotation(Location, TargetPosition);
-
-					static float OrginalPitchMin = Read<float>(PlayerCameraManager + 0x3314); // APlayerCameraManager - ViewPitchMin
-					static float OrginalPitchMax = Read<float>(PlayerCameraManager + 0x3180); // APlayerCameraManager - ViewPitchMax
+			static float OrginalPitchMin = Read<float>(PlayerCameraManager + ViewPitchMin);
+			static float OrginalPitchMax = Read<float>(PlayerCameraManager + ViewPitchMax);
           
-					Write<float>(CurrentWeapon + 0x1088, NewRotation.x);
-					Write<float>(CurrentWeapon + 0x108c, NewRotation.x);
+			Write<float>(CurrentWeapon + 0x1088, NewRotation.x); // APlayerCameraManager - ViewYawMin - 0x331c
+			Write<float>(CurrentWeapon + 0x108c, NewRotation.x); // AFortWeaponRanged - AimPitchMax - 0x10c4
 
-					Write<float>(PlayerCameraManager + 0x3314, NewRotation.y);
-					Write<float>(PlayerCameraManager + 0x3180, NewRotation.y);
+			Write<float>(PlayerCameraManager + ViewPitchMin, NewRotation.y);
+			Write<float>(PlayerCameraManager + ViewPitchMax, NewRotation.y);
 
-					Write<float>(PlayerCameraManager + 0x3314, OrginalPitchMin);
-					Write<float>(PlayerCameraManager + 0x3180, OrginalPitchMax);
-
-			}
+			std::this_thread::sleep_for(std::chrono::seconds(5));
+			
+			Write<float>(PlayerCameraManager + ViewPitchMin, OrginalPitchMin);
+			Write<float>(PlayerCameraManager + ViewPitchMax, OrginalPitchMax);
 		}
 	}
+}
+
+// https://fn.dumps.host/?class=APlayerCameraManager 
+// https://fn.dumps.host/?class=AFortWeaponRanged
